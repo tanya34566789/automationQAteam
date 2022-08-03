fa4e3d058e93352833692d5754fba459336265aa
Tanya <tanyamaystrenko@gmail>
2022-08-03 03:09:18 -0700

    added text to the first test and created the second

--- MyProject/src/test/java/FirstTest.java
+++ MyProject/src/test/java/FirstTest.java
@@ -2,5 +2,8 @@ public class FirstTest {
 
     public static void main(String[] args) {
         System.out.println("Hello Tanya!");
+        System.out.println("Hello Yulia!");
+        System.out.println("Hello Sasha!");
     }
+
 }
