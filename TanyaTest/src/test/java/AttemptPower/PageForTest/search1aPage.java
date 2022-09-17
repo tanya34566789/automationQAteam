9f529096ac064f46c6a168324703055477cc5a8c
Vlad <bwaffle65@gmail.com>
2022-09-17 16:26:25 +0300

    Search1.1a

--- TanyaTest/src/test/java/AttemptPower/PageForTest/search1aPage.java
+++ TanyaTest/src/test/java/AttemptPower/PageForTest/search1aPage.java
@@ -3,28 +3,37 @@ package AttemptPower.PageForTest;
 import FrameworkClasses.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class search1aPage extends BasePage {
-    public search1aPage(WebDriver driver){
+    public search1aPage(WebDriver driver) {
         super(driver);
     }
-    public void goToEpicentr() {
 
-        driver.get("https://epicentrk.ua/ua/shop/compare/");
-    }
-    public WebElement serchField(){
-
+    public WebElement serchField() {
         return findElementByXpath("//input[contains(@class,'_JcImSJ')]");
     }
-    public void findOfficeFurniture() {
 
-        serchField().sendKeys("office furniture\n");
+    public WebElement officeChairs() {
+        return findElementByXpath("//a[contains(@href,'/ua/shop/kompyuternye-kresla-dlya-ofisa/')]");
     }
 
+    public WebElement officeChairsField() {
+        return findElementByXpath("//h1[contains(@class,'nc')]");
+    }
 
+    public void goToEpicentr() {
+        driver.get("https://epicentrk.ua/ua/shop/compare/");
+    }
 
+    public void findOfficeFurniture() {
+        serchField().sendKeys("office furniture\n");
+        Assert.assertTrue(officeChairs().isDisplayed());
+    }
 
-
-
+    public void clickOfficeChairs() {
+        officeChairs().click();
+        Assert.assertTrue(officeChairsField().isDisplayed());
+    }
 
 }
--- TanyaTest/src/test/java/AttemptPower/TestChain/search1aTest.java
+++ TanyaTest/src/test/java/AttemptPower/TestChain/search1aTest.java
@@ -6,9 +6,10 @@ import org.testng.annotations.Test;
 
 public class search1aTest extends TestInit {
     @Test
-    public void search1aEpicentr(){
+    public void search1aEpicentr() {
         search1aPage search1a = new search1aPage(driver);
         search1a.goToEpicentr();
         search1a.findOfficeFurniture();
+        search1a.clickOfficeChairs();
     }
 }
