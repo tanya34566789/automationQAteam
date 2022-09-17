8ab706ec156e887ff81ad9e1947d8ea7dbeac84b
Vlad <bwaffle65@gmail.com>
2022-09-17 17:03:10 +0300

    Search2a

---
+++ TanyaTest/src/test/java/AttemptPower/TestChain/search2aTest.java
@@ -0,0 +1,15 @@
+package AttemptPower.TestChain;
+
+import AttemptPower.PageForTest.search2aPage;
+import FrameworkClasses.TestInit;
+import org.testng.annotations.Test;
+
+public class search2aTest extends TestInit {
+    @Test
+    public void search2aEpicentr() {
+        search2aPage search2a = new search2aPage(driver);
+        search2a.goToEpicentr();
+        search2a.findSomething();
+        search2a.findSomethingTwo();
+    }
+}
