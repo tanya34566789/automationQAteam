97d942f1bcf2ac03e1e90d37812dfe0ca91ee050
Vlad <bwaffle65@gmail.com>
2022-09-19 12:28:40 +0300

    Catalog1a

---
+++ TanyaTest/src/test/java/AttemptPower/TestChain/catalog1aTest.java
@@ -0,0 +1,15 @@
+package AttemptPower.TestChain;
+
+import AttemptPower.PageForTest.catalog1aPage;
+import FrameworkClasses.TestInit;
+import org.testng.annotations.Test;
+
+public class catalog1aTest extends TestInit {
+    @Test
+    public void catalog1aEpicentr() {
+        catalog1aPage catalog1a = new catalog1aPage(driver);
+        catalog1a.goToEpicentr();
+        catalog1a.goCatalog();
+        catalog1a.goTableLamps();
+    }
+}
