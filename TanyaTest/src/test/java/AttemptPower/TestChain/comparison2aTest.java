6651816bd031ba319cb2214f52c77226363712c5
Vlad <bwaffle65@gmail.com>
2022-09-22 15:04:20 +0300

    Comparison2a

---
+++ TanyaTest/src/test/java/AttemptPower/TestChain/comparison2aTest.java
@@ -0,0 +1,16 @@
+package AttemptPower.TestChain;
+
+import AttemptPower.PageForTest.comparison2aPage;
+import FrameworkClasses.TestInit;
+import org.testng.annotations.Test;
+
+public class comparison2aTest extends TestInit {
+    @Test
+    public void comparison2aEpicentr() {
+        comparison2aPage comparison2a = new comparison2aPage(driver);
+        comparison2a.goToEpicentr();
+        comparison2a.goCatalogScrewdrivers();
+        comparison2a.goAddInComparison();
+        comparison2a.goRemoveInComparison();
+    }
+}
