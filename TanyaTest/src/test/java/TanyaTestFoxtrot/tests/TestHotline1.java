755ea7611b26bec481cf204fc066ac703f3b9fcc
tanya34566789 <tanyamaystrenko@gmail.com>
2022-10-05 14:17:26 +0300

    hotline

---
+++ TanyaTest/src/test/java/TanyaTestFoxtrot/tests/TestHotline1.java
@@ -0,0 +1,45 @@
+package TanyaTestFoxtrot.tests;
+
+import TanyaTestFoxtrot.pageObjects.HotlineHomePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class TestHotline extends TestInit {
+    @Test
+    public void cheackHomeSpan() {
+        HotlineHomePage hotlineHomePage = new HotlineHomePage(driver);
+        hotlineHomePage.navigateHotline();
+        ArrayList<String> listOfNames = new ArrayList<>();
+        for (int i = 1; i < hotlineHomePage.categoryNames().size(); i++) {
+            listOfNames.add(hotlineHomePage.categoryNames().get(i).getText());
+        }
+
+        for (int j = 1; j < 8; j++) {
+            hotlineHomePage.categoryNames().get(j).click();
+            Assert.assertEquals(listOfNames.get(j), hotlineHomePage.nameInside().getText());
+            driver.navigate().back();
+        }
+
+    }
+    @Test
+        public void checkCatalogGoods() {
+        HotlineHomePage hotlineHomePage = new HotlineHomePage(driver);
+        hotlineHomePage.navigateHotline();
+        hotlineHomePage.catalog().click();
+        Actions action = new Actions(driver);
+        action.moveToElement(hotlineHomePage.military()).perform();
+        hotlineHomePage.militaryClothes().click();
+        driver.navigate().back();
+        hotlineHomePage.petProducts().click();
+        driver.navigate().back();
+    }
+}
+
+
+
