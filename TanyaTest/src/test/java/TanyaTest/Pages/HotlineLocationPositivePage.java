a7bcd0c9869046d1d3ed019c97bde264e245a84c
tanya34566789 <tanyamaystrenko@gmail.com>
2022-10-11 15:40:00 +0300

    hotline location page

--- TanyaTest/src/test/java/TanyaTest/Pages/HotlineLocationPositivePage.java
+++ TanyaTest/src/test/java/TanyaTest/Pages/HotlineLocationPositivePage.java
@@ -1,2 +1,22 @@
-package TanyaTest.Pages;public class HotlineLocationPositivePage {
+package TanyaTest.Pages;
+
+import FrameworkClasses.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HotlineLocationPositivePage extends BasePage {
+    public HotlineLocationPositivePage(WebDriver driver) {
+
+        super(driver);
+    }
+    public WebElement locationBTN() {
+        return waitElementToBeClickable("//div[@class='header__location-button hidden-below-xl']");
+    }
+     public WebElement locationKyiv() {
+         return waitElementToBeClickable("//span[contains(text(), 'Київ')]");
+     }
+
 }
+
+
+
