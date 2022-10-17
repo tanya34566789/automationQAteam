1a1579a54582499b7ee18695fc7a9438732151e5
tanya34566789 <tanyamaystrenko@gmail.com>
2022-10-17 13:24:20 +0300

    webelemts for SingIn

--- TanyaTest/src/test/java/TanyaTest/Pages/HotlineSingInPage.java
+++ TanyaTest/src/test/java/TanyaTest/Pages/HotlineSingInPage.java
@@ -1,2 +1,31 @@
-package TanyaTest.Pages;public class HotlineSingInPage {
+package TanyaTest.Pages;
+
+import FrameworkClasses.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HotlineSingInPage extends BasePage {
+    public HotlineSingInPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void navigateHotline() {
+
+        driver.get("https://hotline.ua/");
+    }
+
+    public WebElement user() {
+        return waitElementToBeClickable("//div[@class='user-button__image flex middle-xs center-xs']");
+    }
+
+    public WebElement email() {
+       return waitElementToBeClickable("//input[@class='field m_b-sm']");
+    }
+    public WebElement password() {
+       return waitElementToBeClickable("//input[@class='field']");
+    }
+    public WebElement singinBtn() {
+       return waitElementToBeClickable("//input[@class='btn-graphite btn-cell']");
+    }
 }
+
