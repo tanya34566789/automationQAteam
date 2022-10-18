1b565e3a0d4ae44b4f320f307fac1af8bdb09825
tanya34566789 <tanyamaystrenko@gmail.com>
2022-10-18 15:49:05 +0300

    elements for home page

--- TanyaTest/src/test/java/TanyaTest/Pages/UkrNetHomePage.java
+++ TanyaTest/src/test/java/TanyaTest/Pages/UkrNetHomePage.java
@@ -1,2 +1,27 @@
-package TanyaTest.Pages;public class UkrNetHomePage {
+package TanyaTest.Pages;
+
+import FrameworkClasses.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class UkrNetHomePage extends BasePage {
+    public UkrNetHomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void navigateUkrNet() {
+        driver.get("https://www.ukr.net/");
+    }
+
+    public List<WebElement> rightTabs() {
+        return waitVisibilityOfAllElementsLocatedBy("//ul[@class='right-tabs__nav d-clear']");
+    }
+
+    public WebElement searchField() {
+        return waitElementToBeClickable("//input[@id='search-input']");
+    }
 }
+
+
