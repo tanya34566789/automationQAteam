c3cd34b218be3ba856da6e61686442e98fc16be5
tanya34566789 <tanyamaystrenko@gmail.com>
2022-10-18 15:55:25 +0300

    check Headlines

--- TanyaTest/src/test/java/TanyaTest/Tests/UkrNetResultTest.java
+++ TanyaTest/src/test/java/TanyaTest/Tests/UkrNetResultTest.java
@@ -1,9 +1,26 @@
 package TanyaTest.Tests;
 
 import FrameworkClasses.TestInit;
+import TanyaTest.Pages.UkrNetResultPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class UkrNetResultPage extends TestInit {
+public class UkrNetResultTest extends TestInit {
     @Test
-    public void checkResult
+    public void checkHeadlines() {
+        UkrNetResultPage ukrNetResultPage = new UkrNetResultPage(driver);
+        ukrNetResultPage.navigateUkrNet();
+        ukrNetResultPage.buttonMain().click();
+        driver.navigate().back();
+        ukrNetResultPage.buttonPolitika().click();
+        driver.navigate().back();
+        ukrNetResultPage.buttonEkonomika().click();
+        driver.navigate().back();
+        ukrNetResultPage.buttonEvents().click();
+        driver.navigate().back();
+        Assert.assertTrue(ukrNetResultPage.buttonMain().isDisplayed());
+        Assert.assertTrue(ukrNetResultPage.buttonPolitika().isDisplayed());
+        Assert.assertTrue(ukrNetResultPage.buttonEvents().isDisplayed());
+        Assert.assertTrue(ukrNetResultPage.buttonEkonomika().isDisplayed());
+    }
 }
