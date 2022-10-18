7ea8cf1f2640aaa3e508a915a0514742a914800c
tanya34566789 <tanyamaystrenko@gmail.com>
2022-10-18 15:47:14 +0300

    check home page

---
+++ TanyaTest/src/test/java/TanyaTest/Pages/FoxtrotHomePage.java
@@ -0,0 +1,2 @@
+package TanyaTest.Pages;public class FoxtrotHomePage {
+}
---
+++ TanyaTest/src/test/java/TanyaTest/Tests/UkrNetResultTest.java
@@ -0,0 +1,9 @@
+package TanyaTest.Tests;
+
+import FrameworkClasses.TestInit;
+import org.testng.annotations.Test;
+
+public class UkrNetResultPage extends TestInit {
+    @Test
+    public void checkResult
+}
