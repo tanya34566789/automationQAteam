776411c0e45dcaa0924570da49e4d442d3b7a3a7
tanya34566789 <tanyamaystrenko@gmail.com>
2022-10-21 11:06:13 +0300

    elements for AlloTest

--- TanyaTest/src/test/java/TanyaTest/Pages/AlloPage.java
+++ TanyaTest/src/test/java/TanyaTest/Pages/AlloPage.java
@@ -1,2 +1,51 @@
-package TanyaTest.Pages;public class AlloPage {
+package TanyaTest.Pages;
+
+import FrameworkClasses.BasePage;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class AlloPage extends BasePage {
+    public AlloPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void navigateAllo() {
+        driver.get("https://allo.ua/");
+    }
+
+    public WebElement buttonShops() {
+        return waitElementToBeClickable("//div[@class='mh-links']//a[contains(text(), 'Магазини')]");
+    }
+
+    public WebElement buttonKyiv() {
+        return waitElementToBeClickable("//div[@class='mh-links']//a[contains(text(), 'Магазини')]");
+    }
+
+    public WebElement buttonFilter() {
+        return waitElementToBeClickable("//button[@title='Показати таблицю']");
+    }
+
+    public void scroll(int pixels) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0," + pixels + ")", "");
+    }
+    public WebElement buttonShares() {
+        return waitElementToBeClickable("//div[@class='mh-links']//a[contains(text(), 'Акції')]");
+    }
+    public WebElement buttonPresents() {
+        return waitElementToBeClickable("//div[@class='promo-list__categories']//span[contains(text(), 'Подарунки')]");
+    }
+    public WebElement buttonAirСonditioners() {
+        return waitElementToBeClickable("//div[@class='promo-list__items']//img[@alt='На кондиціонери Midea знижка на встановлення до 50% + Wi-Fi модуль!']");
+    }
+    public List<WebElement> buttonCompare() {
+
+        return waitVisibilityOfAllElementsLocatedBy("//div[@class='compare']");
+    }
+    public WebElement Compare() {
+        return waitElementToBeClickable("//a[@class='mh-button mh-compare']");
+    }
 }
