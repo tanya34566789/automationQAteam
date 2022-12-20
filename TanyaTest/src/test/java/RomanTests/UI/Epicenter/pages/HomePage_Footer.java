52170c599ae75d67288b1c94889b3416b5112494
Roman Martsenko <romkamartcenko@gmail.com>
2022-12-20 16:59:55 +0200

    the class HomePage_Footer is created and ready

--- TanyaTest/src/test/java/RomanTests/UI/Epicenter/pages/HomePage_Footer.java
+++ TanyaTest/src/test/java/RomanTests/UI/Epicenter/pages/HomePage_Footer.java
@@ -1,12 +1,20 @@
 package RomanTests.UI.Epicenter.pages;
 
 import TanyaTestFoxtrot.pageObjects.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HomePage_Footer extends BasePage {
     public HomePage_Footer(WebDriver driver) {
         super(driver);
     }
 
+public WebElement socialNetworksContainer(){
+        return driver.findElement(By.id("//div[contains(@class, 'footer__blue')]"));
+}
 
+public WebElement footerContainer(){
+        return driver.findElement(By.id("//div[contains(@class, 'footer__info')]"));
+}
 }
