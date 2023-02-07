8eb5ba94c0fdc1648ed77612a5c69115507a60d7
Roman Martsenko <romkamartcenko@gmail.com>
2023-02-07 13:53:27 +0200

    try export json from postman

--- TanyaTest/src/test/java/RomanTests/API/Swagger/Tests/ApiTests.java
+++ TanyaTest/src/test/java/RomanTests/API/Swagger/Tests/ApiTests.java
@@ -1,9 +1,17 @@
 package RomanTests.API.Swagger.Tests;
 
 import RomanTests.API.Swagger.Base.Base;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 import org.testng.annotations.Test;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -18,6 +26,16 @@ public class ApiTests{
                 .when()
                 .get("app/videogames")
                 .getBody();
+    }
+
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode root = mapper.readTree(new File("/Users/personal/Desktop/QA/automationQAteam/TanyaTest/src/test/java/RomanTests/API/Swagger/Tests/data.json"));
+            // Use the data in root as needed in your code
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
